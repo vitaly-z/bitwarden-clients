23f634e50e976ffb6c9876d297694a53ef608f33
Jared Snider <jsnider@bitwarden.com>
2023-05-02 16:56:31 -0400

    PM-1378 - CryptoService makeDeviceKey test written

--- libs/common/spec/services/crypto.service.spec.ts
+++ libs/common/spec/services/crypto.service.spec.ts
@@ -5,7 +5,9 @@ import { EncryptService } from "@bitwarden/common/abstractions/encrypt.service";
 import { LogService } from "@bitwarden/common/abstractions/log.service";
 import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
 import { StateService } from "@bitwarden/common/abstractions/state.service";
+import { SymmetricCryptoKey } from "@bitwarden/common/models/domain/symmetric-crypto-key";
 import { CryptoService } from "@bitwarden/common/services/crypto.service";
+import { CsprngArray } from "@bitwarden/common/types/csprng";
 
 describe("cryptoService", () => {
   let cryptoService: CryptoService;
@@ -15,6 +17,8 @@ describe("cryptoService", () => {
   const platformUtilService = mock<PlatformUtilsService>();
   const logService = mock<LogService>();
   const stateService = mock<StateService>();
+
+  const deviceKeyBytesLength = 64;
 
   beforeEach(() => {
     mockReset(cryptoFunctionService);
@@ -34,5 +38,23 @@ describe("cryptoService", () => {
 
   it("instantiates", () => {
     expect(cryptoService).not.toBeFalsy();
+  });
+
+  it("makeDeviceKey functions per requirements: (1) creates 512 bit device encryption key (2) securely stores it", async () => {
+    const mockRandomBytes = new Uint8Array(deviceKeyBytesLength).buffer as CsprngArray;
+
+    const cryptoFuncSvcRandomBytesSpy = jest
+      .spyOn(cryptoFunctionService, "randomBytes")
+      .mockResolvedValue(mockRandomBytes);
+    const stateSvcSetDeviceKeySpy = jest.spyOn(stateService, "setDeviceKey");
+
+    const deviceKey = await cryptoService.makeDeviceKey();
+
+    expect(cryptoFuncSvcRandomBytesSpy).toHaveBeenCalledTimes(1);
+    expect(cryptoFuncSvcRandomBytesSpy).toHaveBeenCalledWith(deviceKeyBytesLength);
+    expect(deviceKey).not.toBeNull();
+    expect(deviceKey).toBeInstanceOf(SymmetricCryptoKey);
+    expect(stateSvcSetDeviceKeySpy).toHaveBeenCalledTimes(1);
+    expect(stateSvcSetDeviceKeySpy).toHaveBeenCalledWith(deviceKey);
   });
 });
