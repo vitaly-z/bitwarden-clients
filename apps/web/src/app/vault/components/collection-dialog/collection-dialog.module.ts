bd8863049eb7d217e31312fdb00a6ea5116cec1d
Robyn MacCallum <robyntmaccallum@gmail.com>
2023-05-03 13:02:50 -0400

    Fix CollectionDialogModule imports

--- apps/web/src/app/vault/components/collection-dialog/collection-dialog.module.ts
+++ apps/web/src/app/vault/components/collection-dialog/collection-dialog.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from "@angular/core";
 
 import { SelectModule } from "@bitwarden/components";
 
-import { AccessSelectorModule } from "../../../../../admin-console/organizations/shared/components/access-selector/access-selector.module";
-import { SharedModule } from "../../../../../shared";
+import { CoreOrganizationModule } from "../../../admin-console/organizations/core";
+import { AccessSelectorModule } from "../../../admin-console/organizations/shared/components/access-selector/access-selector.module";
+import { SharedModule } from "../../../shared";
 
 import { CollectionDialogComponent } from "./collection-dialog.component";
-
 @NgModule({
-  imports: [SharedModule, AccessSelectorModule, SelectModule],
+  imports: [SharedModule, AccessSelectorModule, SelectModule, CoreOrganizationModule],
   declarations: [CollectionDialogComponent],
   exports: [CollectionDialogComponent],
 })
