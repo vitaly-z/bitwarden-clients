c0bd70311fb569bfb8dd0618ae6f0c9d8f9943ea
Jared Snider <jsnider@bitwarden.com>
2023-05-08 14:37:33 -0400

    PM-1378- Updating makeDeviceKey --> getDeviceKey tests to match updated code

--- libs/common/spec/services/crypto.service.spec.ts
+++ libs/common/spec/services/crypto.service.spec.ts
@@ -38,25 +38,61 @@ describe("cryptoService", () => {
     expect(cryptoService).not.toBeFalsy();
   });
 
-  describe("makeDeviceKey", () => {
+  describe("getDeviceKey", () => {
     const deviceKeyBytesLength = 64;
 
-    it("Creates a non-null 64 byte device encryption key and securely stores it", async () => {
+    it("creates and returns a new non-null 64 byte device encryption key and securely stores it if not already stored", async () => {
       const mockRandomBytes = new Uint8Array(deviceKeyBytesLength).buffer as CsprngArray;
 
       const cryptoFuncSvcRandomBytesSpy = jest
         .spyOn(cryptoFunctionService, "randomBytes")
         .mockResolvedValue(mockRandomBytes);
+
+      // Return null to simulate no device key stored
+      const stateSvcGetDeviceKeySpy = jest
+        .spyOn(stateService, "getDeviceKey")
+        .mockResolvedValue(null);
+
       const stateSvcSetDeviceKeySpy = jest.spyOn(stateService, "setDeviceKey");
 
       const deviceKey = await cryptoService.getDeviceKey();
 
+      expect(stateSvcGetDeviceKeySpy).toHaveBeenCalledTimes(1);
+
       expect(cryptoFuncSvcRandomBytesSpy).toHaveBeenCalledTimes(1);
       expect(cryptoFuncSvcRandomBytesSpy).toHaveBeenCalledWith(deviceKeyBytesLength);
+
       expect(deviceKey).not.toBeNull();
       expect(deviceKey).toBeInstanceOf(SymmetricCryptoKey);
+
       expect(stateSvcSetDeviceKeySpy).toHaveBeenCalledTimes(1);
       expect(stateSvcSetDeviceKeySpy).toHaveBeenCalledWith(deviceKey);
+    });
+
+    it("returns the existing device key without creating a new one if already stored", async () => {
+      const existingDeviceKey = new SymmetricCryptoKey(
+        new Uint8Array(deviceKeyBytesLength).buffer as CsprngArray
+      );
+
+      const cryptoFuncSvcRandomBytesSpy = jest.spyOn(cryptoFunctionService, "randomBytes");
+
+      // Return existing device key to simulate already stored
+      const stateSvcGetDeviceKeySpy = jest
+        .spyOn(stateService, "getDeviceKey")
+        .mockResolvedValue(existingDeviceKey);
+
+      const stateSvcSetDeviceKeySpy = jest.spyOn(stateService, "setDeviceKey");
+
+      const deviceKey = await cryptoService.getDeviceKey();
+
+      expect(stateSvcGetDeviceKeySpy).toHaveBeenCalledTimes(1);
+      expect(cryptoFuncSvcRandomBytesSpy).not.toHaveBeenCalled();
+
+      expect(deviceKey).not.toBeNull();
+      expect(deviceKey).toBeInstanceOf(SymmetricCryptoKey);
+      expect(deviceKey).toEqual(existingDeviceKey);
+
+      expect(stateSvcSetDeviceKeySpy).not.toHaveBeenCalled();
     });
   });
 });
